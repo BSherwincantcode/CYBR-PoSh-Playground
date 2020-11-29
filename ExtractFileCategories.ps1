36cd1e113db3bc0233c45f15524c7b33f941ed70
BSherwincantcode <74805002+BSherwincantcode@users.noreply.github.com>
2020-11-29 17:43:31 -0500

    Create ExtractFileCategories.ps1
    Consumes Policies.XML
    Outputs a CSV of PolicyIDs and associated Properties

---
+++ ExtractFileCategories.ps1
@@ -0,0 +1,29 @@
+$xmlpath = "C:\Users\Administrator\Downloads\HealthCheck_0825\HealthCheck_0825\Policies.xml"
+[xml]$policies = Get-Content $xmlpath
+$xpath = 'PasswordVaultPolicies/Devices/Device/Policies/Policy'
+$propsTable = @{}
+$allprops = @()
+$ExportDIR = "C:\Users\Administrator\Documents\"
+$ExportFile = $ExportDIR + "PoliciesXML_PropsAnalaysis_" + (Get-Date -Format "MM-dd HHMM") + ".csv"
+$Newfile = @(
+    [PSCustomObject]@{
+        PolicyID=[String]''
+        Property=[String]''
+    }
+)
+
+
+foreach($Policy in $policies.PasswordVaultPolicies.Devices.Device.Policies.Policy){ 
+
+    [array]$props = ( $Policy.Properties.Required.Property | Select-Object -ExpandProperty Name )
+    $props += ( $Policy.Properties.Optional.Property | Select-Object -ExpandProperty Name )
+    $propstable.add($policy.ID,$props)
+   
+}
+
+Add-Content -Path $ExportFile -Value "PolicyID,Property"
+ForEach($ID in $propsTable.Keys){
+    Foreach($prop in $propsTable.$ID){
+        Add-Content -Path $ExportFile -Value ( $ID + "," + $prop )
+    }
+}
